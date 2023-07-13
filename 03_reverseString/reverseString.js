1838f960cab421ee28eae9bbba8752bd42d7e71b
Tony <39646692+PrinceORS@users.noreply.github.com>
2023-07-13 12:07:40 +0200

    Add files via upload

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+	let string = str;
+	let newString  = '';
+	for(let i = string.length-1; i >= 0; i--){
+		newString += string[i];
+	}
+	return newString;
 };
+
+string = ['hello', 'hello there', '123! abc!', ''];
+
+for(let j = 0; j <= string.length; j++){
+	reverseString('hello');
+}
 
 // Do not edit below this line
 module.exports = reverseString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
-};
+const sumAll = function(loNum, hiNum){
+	let sum = 0;
+	if(isNaN(loNum) || isNaN(hiNum)){
+		return 'ERROR';
+	}else if(typeof loNum == 'string' || typeof hiNum == 'string'){
+		return 'ERROR';
+	}else if(loNum < 0 || hiNum < 0){
+		return 'ERROR';
+	}else if(loNum < hiNum){
+		for(let i = loNum; i <= hiNum; i++)
+			sum += i;
+	}else{
+		// High number first
+		for(let i = hiNum; i <= loNum; i++){
+			sum += i;
+	}
+	}
+	return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
